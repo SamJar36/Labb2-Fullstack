f8d6c35c163b31e60bb236107c716bd24d12eb6b
SamJar36 <samandreasjarva@gmail.com>
2025-03-27 15:32:21 +0100

    Add more stuff

--- BlazorWebApp/Program.cs
+++ BlazorWebApp/Program.cs
@@ -4,9 +4,9 @@ using BlazorWebApp;
 using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 
 builder.Services.AddHttpClient();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
