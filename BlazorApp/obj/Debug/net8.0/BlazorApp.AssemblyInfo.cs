999c273eb259aa901918cf21306d169bd0a65c9f
SamJar36 <samandreasjarva@gmail.com>
2025-03-31 11:19:14 +0200

    Add delete customer

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8d6c35c163b31e60bb236107c716bd24d12eb6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0d814c8e16555328326062f6c34215cc1c95cfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
