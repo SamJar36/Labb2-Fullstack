99c0ccaa16f1ee39fc121229dc3c5e0d47b4a538
SamJar36 <samandreasjarva@gmail.com>
2025-03-31 14:52:41 +0200

    Add create page

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+999c273eb259aa901918cf21306d169bd0a65c9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab9282ec45f74e0db398c6a3eab4c5a45f08eacf")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
