36c854f38af460691f5d1ac20a5ca0c6bd1ede8a
SamJar36 <samandreasjarva@gmail.com>
2025-04-03 10:37:24 +0200

    Add edit and delete on customer search

--- Labb2-Fullstack/obj/Debug/net8.0/Labb2-REST API.AssemblyInfo.cs
+++ Labb2-Fullstack/obj/Debug/net8.0/Labb2-REST API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Labb2-REST API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fb0ee8f8ea0844783c8502a4dd0bb2d61c0f1a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+403bbc0f518896fd2d446e084304551ef3178f4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Labb2-REST API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Labb2-REST API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
