d69fb36fac029e73564d31178effe3765ccdb629
SamJar36 <samandreasjarva@gmail.com>
2025-04-03 11:02:20 +0200

    Add edit products

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+403bbc0f518896fd2d446e084304551ef3178f4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36c854f38af460691f5d1ac20a5ca0c6bd1ede8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
